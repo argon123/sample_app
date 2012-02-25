532bd42c811e424f6f6b13d153c98737aeedf59e
argon123 <arthuri4ion@gmail.com>
2012-02-26 02:26:48 +0400

    helper-css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #retutn title 
+  def title
+      base_title = "Ruby on rails tutorial somple app!)"
+      if @title.nil?
+	base_title
+      else
+	"#{base_title} | #{@title}"
+      end
+  end
 end
